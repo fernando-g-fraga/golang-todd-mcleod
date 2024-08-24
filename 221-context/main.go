334963c1febe20f294ad9ac88f5cc128af26868b
fernando fraga <fernandogrfraga@gmail.com>
2024-08-24 12:45:43 -0300

    Understanding context

--- 221-context/main.go
+++ 221-context/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+)
 
+func main() {
+	ctx := context.Background()
+
+	fmt.Println("context:\t", ctx)
+	fmt.Println("context err:\t", ctx.Err())
+	fmt.Printf("context type:\t%T\n", ctx)
+	fmt.Println("----------")
+
+	ctx, _ = context.WithCancel(ctx)
+
+	fmt.Println("context:\t", ctx)
+	fmt.Println("context err:\t", ctx.Err())
+	fmt.Printf("context type:\t%T\n", ctx)
+	fmt.Println("----------")
+
+	ctx, cancel := context.WithCancel(ctx)
+
+	fmt.Println("context:\t", ctx)
+	fmt.Println("context err:\t", ctx.Err())
+	fmt.Printf("context type:\t%T\n", ctx)
+	fmt.Println("----------")
+
+	cancel()
 }
